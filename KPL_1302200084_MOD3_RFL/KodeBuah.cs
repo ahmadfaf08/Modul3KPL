6e9b65a618aa1246fd688d83e42ef4e280880b75
ahmadfaf08 <afafgaming123@gmail.com>
2022-03-12 16:03:39 +0700

    menambahkan class KodeBuah

---
+++ KPL_1302200084_MOD3_RFL/KodeBuah.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KPL_1302200084_MOD3_RFL
+{
+    internal class KodeBuah
+    {
+        public static void getKodeBuah(string buah, string kode)
+        {
+            Console.WriteLine(buah + " " + kode);
+        }
+    }
+}
--- KPL_1302200084_MOD3_RFL/Program.cs
+++ KPL_1302200084_MOD3_RFL/Program.cs
@@ -1,2 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace KPL_1302200084_MOD3_RFL
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            KodeBuah.getKodeBuah("Nama Buah", "   Kode Buah");
+            KodeBuah.getKodeBuah("Apel\t", "\t A00");
+            KodeBuah.getKodeBuah("Aprikot\t", "\t B00");
+            KodeBuah.getKodeBuah("Alpukat\t", "\t C00");
+            KodeBuah.getKodeBuah("Pisang\t", "\t D00");
+            KodeBuah.getKodeBuah("Paprika\t", "\t E00");
+            KodeBuah.getKodeBuah("Blackberry\t", "F00");
+            KodeBuah.getKodeBuah("Ceri\t", "\t H00");
+            KodeBuah.getKodeBuah("Kelapa\t", "\t I00");
+            KodeBuah.getKodeBuah("Jagung\t", "\t J00");
+            KodeBuah.getKodeBuah("Kurma\t", "\t K00");
+            KodeBuah.getKodeBuah("Durian\t", "\t L00");
+            KodeBuah.getKodeBuah("Anggur\t", "\t M00");
+            KodeBuah.getKodeBuah("Melon\t", "\t N00");
+            KodeBuah.getKodeBuah("Semangka\t", "O00");
+        }
+    }
+}
